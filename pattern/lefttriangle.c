82ae11b02215ab1f657b86b0e934f2771abac80f
sneha-vishwakarma <snehavishwakarma728@gmail.com>
2025-10-06 15:01:48 +0530

    added patterns

---
+++ pattern/lefttriangle.c
@@ -0,0 +1,20 @@
+//left triangle
+#include<stdio.h>
+int main()
+ {  
+    int i,j,n=5;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+       printf("\n");
+    }
+      return 0;
+ }
+
+
+
+
+
+
+
